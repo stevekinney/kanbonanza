2818fad851384f2b4b9c16f88cb4244044635df6
Steve Kinney <hello@stevekinney.net>
2019-05-19 18:14:12 -0600

    Use object notation for mapDispatchToProps

--- src/containers/CreateListContainer.js
+++ src/containers/CreateListContainer.js
@@ -1,11 +1,10 @@
 import { connect } from 'react-redux';
 import CreateList from '../components/CreateList';
-import { bindActionCreators } from 'redux'
 
 import { createList } from '../actions/list';
 
-const mapDispatchToProps = (dispatch) => {
-  return bindActionCreators({ createList }, dispatch);
+const mapDispatchToProps = {
+  createList
 };
 
 export default connect(null, mapDispatchToProps)(CreateList);
